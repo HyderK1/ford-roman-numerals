08617ad12ee52e96fa50a5cdac2f72d3342cd9a3
Hyder <hyderk32@gmail.com>
2022-07-27 01:54:09 +0100

    FORD-TASK: ✅ Add hashmap with base case roman numerals (fix test)

--- src/main/java/RomanNumeralService.java
+++ src/main/java/RomanNumeralService.java
@@ -1,13 +1,38 @@
+import java.util.HashMap;
+
 public class RomanNumeralService {
+
+    final HashMap<Integer, String> romanNumerals;
+
+    public RomanNumeralService() {
+        this.romanNumerals = buildRomanNumerals();
+    }
 
     public String intToRomanNumeral(int num){
 
         if (num < 1 || num > 3000)
             throw new IllegalArgumentException("The number provided (%d) is not in the range 1-3000".formatted(num));
-        return null;
+
+        return romanNumerals.get(num);
     }
 
     public int romanNumeralToInt(String romanNumeral){
         return -1;
+    }
+
+
+    private HashMap<Integer, String> buildRomanNumerals() {
+        final var romanNumerals = new HashMap<Integer, String>();
+
+        romanNumerals.put(0, "");
+        romanNumerals.put(1, "I");
+        romanNumerals.put(5, "V");
+        romanNumerals.put(10, "X");
+        romanNumerals.put(50, "L");
+        romanNumerals.put(100, "C");
+        romanNumerals.put(500, "D");
+        romanNumerals.put(1000, "M");
+
+        return romanNumerals;
     }
 }
