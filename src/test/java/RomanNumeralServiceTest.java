d04f95a321442ad0abf356b97f85281810696fb3
Hyder <hyderk32@gmail.com>
2022-07-27 01:44:21 +0100

    FORD-TASK: 🧪 Add test for base cases of roman numerals (Test failing)

--- src/test/java/RomanNumeralServiceTest.java
+++ src/test/java/RomanNumeralServiceTest.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class RomanNumeralServiceTest {
@@ -22,5 +23,17 @@ class RomanNumeralServiceTest {
                     .isInstanceOf(IllegalArgumentException.class)
                     .hasMessage("The number provided (%s) is not in the range 1-3000".formatted(num));
     }
+
+    @Test
+    public void romanNumeralToInt_shouldReturnRomanNumeralForKeyIntegers() {
+        assertThat(romanNumeralService.intToRomanNumeral(1)).isEqualTo("I");
+        assertThat(romanNumeralService.intToRomanNumeral(5)).isEqualTo("V");
+        assertThat(romanNumeralService.intToRomanNumeral(10)).isEqualTo("X");
+        assertThat(romanNumeralService.intToRomanNumeral(50)).isEqualTo("L");
+        assertThat(romanNumeralService.intToRomanNumeral(100)).isEqualTo("C");
+        assertThat(romanNumeralService.intToRomanNumeral(500)).isEqualTo("D");
+        assertThat(romanNumeralService.intToRomanNumeral(1000)).isEqualTo("M");
+    }
+
 
 }
